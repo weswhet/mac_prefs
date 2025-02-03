c3bccffd701f84b01c41eb257e23e8635ad246f0
Wesley Whetstone <jckwhet@gmail.com>
2025-02-02 18:12:24 -0800

    Update SetValue and GetValue to use PreferenceScope

--- mac_prefs/mac_prefs.go
+++ mac_prefs/mac_prefs.go
@@ -10,15 +10,40 @@ import (
 	"strings"
 )
 
-const (
-	CurrentUser = "kCFPreferencesCurrentUser"
-	AnyUser     = "kCFPreferencesAnyUser"
-	CurrentHost = "kCFPreferencesCurrentHost"
-	AnyHost     = "kCFPreferencesAnyHost"
+// UserType represents the type of user for preferences
+type UserType string
+
+// HostType represents the type of host for preferences
+type HostType string
+
+// PreferenceScope defines the scope for preferences
+type PreferenceScope struct {
+	User UserType
+	Host HostType
+}
+
+var (
+	// CurrentUser represents the current user's preferences
+	CurrentUser UserType = "kCFPreferencesCurrentUser"
+	// AnyUser represents preferences for any user
+	AnyUser UserType = "kCFPreferencesAnyUser"
+	// CurrentHost represents the current host's preferences
+	CurrentHost HostType = "kCFPreferencesCurrentHost"
+	// AnyHost represents preferences for any host
+	AnyHost HostType = "kCFPreferencesAnyHost"
+
+	// CurrentUserCurrentHost represents preferences for the current user on the current host
+	CurrentUserCurrentHost = PreferenceScope{User: CurrentUser, Host: CurrentHost}
+	// CurrentUserAnyHost represents preferences for the current user on any host
+	CurrentUserAnyHost = PreferenceScope{User: CurrentUser, Host: AnyHost}
+	// AnyUserCurrentHost represents preferences for any user on the current host
+	AnyUserCurrentHost = PreferenceScope{User: AnyUser, Host: CurrentHost}
+	// AnyUserAnyHost represents preferences for any user on any host
+	AnyUserAnyHost = PreferenceScope{User: AnyUser, Host: AnyHost}
 )
 
-// SetValue sets a preference value for the given key, application ID, user, and host.
-func SetValue(key string, value interface{}, applicationID string, userName string, hostName string) error {
+// Set sets a preference value for the given key, application ID, and preference scope.
+func Set(key string, value interface{}, applicationID string, scope PreferenceScope) error {
 	cKey, err := StringToCFString(key)
 	if err != nil {
 		return fmt.Errorf("error creating CFString for key: %v", err)
@@ -40,23 +65,23 @@ func SetValue(key string, value interface{}, applicationID string, userName stri
 	defer Release(C.CFTypeRef(cAppID))
 
 	var cUserName C.CFStringRef
-	switch strings.ToLower(userName) {
-	case strings.ToLower(CurrentUser):
+	switch scope.User {
+	case CurrentUser:
 		cUserName = C.kCFPreferencesCurrentUser
-	case strings.ToLower(AnyUser):
+	case AnyUser:
 		cUserName = C.kCFPreferencesAnyUser
 	default:
-		return fmt.Errorf("invalid userName: must be CurrentUser or AnyUser")
+		return fmt.Errorf("invalid user type in scope: must be CurrentUser or AnyUser")
 	}
 
 	var cHostName C.CFStringRef
-	switch strings.ToLower(hostName) {
-	case strings.ToLower(CurrentHost):
+	switch scope.Host {
+	case CurrentHost:
 		cHostName = C.kCFPreferencesCurrentHost
-	case strings.ToLower(AnyHost):
+	case AnyHost:
 		cHostName = C.kCFPreferencesAnyHost
 	default:
-		return fmt.Errorf("invalid hostName: must be CurrentHost or AnyHost")
+		return fmt.Errorf("invalid host type in scope: must be CurrentHost or AnyHost")
 	}
 
 	C.CFPreferencesSetValue(cKey, cValue, cAppID, cUserName, cHostName)
@@ -69,37 +94,14 @@ func SetValue(key string, value interface{}, applicationID string, userName stri
 	return nil
 }
 
-// Set sets a preference value for the given key and application ID.
-func Set(key string, value interface{}, appID string) error {
-	cKey, err := StringToCFString(key)
-	if err != nil {
-		return fmt.Errorf("error creating CFString for key: %v", err)
-	}
-	defer Release(C.CFTypeRef(cKey))
-
-	cAppID, err := StringToCFString(appID)
-	if err != nil {
-		return fmt.Errorf("error creating CFString for appID: %v", err)
-	}
-	defer Release(C.CFTypeRef(cAppID))
-
-	cValue, err := ConvertToCFType(value)
-	if err != nil {
-		return fmt.Errorf("error converting value to CFType: %v", err)
-	}
-	defer Release(cValue)
-
-	C.CFPreferencesSetAppValue(cKey, cValue, cAppID)
-	success := C.CFPreferencesAppSynchronize(cAppID)
-	if success == C.false {
-		return fmt.Errorf("failed to synchronize preferences")
-	}
-
-	return nil
+// SetValue sets a preference value for the given key and application ID.
+// This function uses the CurrentUserCurrentHost scope.
+func SetValue(key string, value interface{}, appID string) error {
+	return Set(key, value, appID, CurrentUserCurrentHost)
 }
 
-// GetValue retrieves a preference value for the given key, application ID, user, and host.
-func GetValue(key string, applicationID string, userName string, hostName string) (interface{}, error) {
+// Get retrieves a preference value for the given key, application ID, and preference scope.
+func Get(key string, applicationID string, scope PreferenceScope) (interface{}, error) {
 	cKey, err := StringToCFString(key)
 	if err != nil {
 		return nil, fmt.Errorf("error creating CFString for key: %v", err)
@@ -113,23 +115,23 @@ func GetValue(key string, applicationID string, userName string, hostName string
 	defer Release(C.CFTypeRef(cAppID))
 
 	var cUserName C.CFStringRef
-	switch strings.ToLower(userName) {
-	case strings.ToLower(CurrentUser):
+	switch scope.User {
+	case CurrentUser:
 		cUserName = C.kCFPreferencesCurrentUser
-	case strings.ToLower(AnyUser):
+	case AnyUser:
 		cUserName = C.kCFPreferencesAnyUser
 	default:
-		return nil, fmt.Errorf("invalid userName: must be CurrentUser or AnyUser")
+		return nil, fmt.Errorf("invalid user type in scope: must be CurrentUser or AnyUser")
 	}
 
 	var cHostName C.CFStringRef
-	switch strings.ToLower(hostName) {
-	case strings.ToLower(CurrentHost):
+	switch scope.Host {
+	case CurrentHost:
 		cHostName = C.kCFPreferencesCurrentHost
-	case strings.ToLower(AnyHost):
+	case AnyHost:
 		cHostName = C.kCFPreferencesAnyHost
 	default:
-		return nil, fmt.Errorf("invalid hostName: must be CurrentHost or AnyHost")
+		return nil, fmt.Errorf("invalid host type in scope: must be CurrentHost or AnyHost")
 	}
 
 	value := C.CFPreferencesCopyValue(cKey, cAppID, cUserName, cHostName)
@@ -141,25 +143,8 @@ func GetValue(key string, applicationID string, userName string, hostName string
 	return ConvertFromCFType(value)
 }
 
-// Get retrieves a preference value for the given key and application ID.
-func Get(key string, appID string) (interface{}, error) {
-	cKey, err := StringToCFString(key)
-	if err != nil {
-		return nil, fmt.Errorf("error creating CFString for key: %v", err)
-	}
-	defer Release(C.CFTypeRef(cKey))
-
-	cAppID, err := StringToCFString(appID)
-	if err != nil {
-		return nil, fmt.Errorf("error creating CFString for appID: %v", err)
-	}
-	defer Release(C.CFTypeRef(cAppID))
-
-	value := C.CFPreferencesCopyAppValue(cKey, cAppID)
-	if value == NilCFType {
-		return nil, nil // Preference not found
-	}
-	defer Release(C.CFTypeRef(value))
-
-	return ConvertFromCFType(value)
+// GetValue retrieves a preference value for the given key and application ID.
+// This function uses the CurrentUserCurrentHost scope.
+func GetValue(key string, appID string) (interface{}, error) {
+	return Get(key, appID, CurrentUserCurrentHost)
 }
